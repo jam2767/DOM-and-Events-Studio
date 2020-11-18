c89b95763163dc3c0bc24ccfa291e4f0b2e33c8b
Joshua Mantooth <joshua.mantooth@joshua.mantooth>
2020-11-18 00:36:44 -0600

    add window load listener and interact with takeoff button

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function(){
+
+    document.getElementById("takeoff").addEventListener("click", function(){
+        shuttleReady = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        
+        if (shuttleReady = true){
+            document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
+            document.getElementById("shuttleBackground".innerHTML = )
+        } else if (shuttleReady = false){
+            document.getElementById("flightStatus").innerHTML = "Space shuttle ready for takeoff";
+        }
+    }); 
+
+
+
+
+});
+
+//let takeoff = document.getElementById("takeoff");
+
+
