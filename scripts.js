c3f9a26a72aeb099ada991fe80cd0d1973cded11
Joshua Mantooth <joshua.mantooth@joshua.mantooth>
2020-11-23 02:40:42 -0600

    added requirements 1-4

--- scripts.js
+++ scripts.js
@@ -1,22 +1,48 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-window.addEventListener("load", function(){
-
+function init(){
     document.getElementById("takeoff").addEventListener("click", function(){
-        shuttleReady = window.confirm("Confirm that the shuttle is ready for takeoff.");
-        
-        if (shuttleReady = true){
+        if(window.confirm("Confirm that the shuttle is ready for takeoff.")){
             document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
-            document.getElementById("shuttleBackground".innerHTML = )
-        } else if (shuttleReady = false){
+            document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+            shuttleHeight = document.getElementById("spaceShuttleHeight").innerHTML;
+            newHeight = parseInt(shuttleHeight,10)+10000;
+            document.getElementById("spaceShuttleHeight").innerHTML = newHeight;
+
+        } else {
             document.getElementById("flightStatus").innerHTML = "Space shuttle ready for takeoff";
+            document.getElementById("shuttleBackground").style.backgroundColor = "green";
+
         }
+        
     }); 
 
+    document.getElementById("landing").addEventListener("click", function(){
+        if(window.confirm("The shuttle is landing. Landing gear engaged.")){
+            document.getElementById("flightStatus").innerHTML = "The shuttle has landed";
+            document.getElementById("shuttleBackground").style.backgroundColor = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = 0;
+        }
+    })
 
+    document.getElementById("missionAbort").addEventListener("click", function(){
+        if(window.confirm("Confirm that you want to abort the mission")){
+            document.getElementById("flightStatus").innerHTML = "Mission aborted";
+            document.getElementById("shuttleBackground").style.backgroundColor = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = 0;
+        }
+    })
 
+    document.getElementById("up").addEventListener("click", function(){
+        shuttleImgUp = document.querySelector("img");
+        console.log(shuttleImgUp);
+        shuttleImgUp = "10px";
+        document.querySelector("img").style.bottom = shuttleImgUp;
 
-});
+    })
+};
+
+window.onload = init;
 
 //let takeoff = document.getElementById("takeoff");
 
